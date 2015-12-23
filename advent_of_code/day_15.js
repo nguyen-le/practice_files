094252c5756e74232d045d6dd0a9a1af2531363b
Nguyen Le <nguyentle7@gmail.com>
2015-12-22 23:16:03 -0800

    day 15 advent complete

--- advent_of_code/day_15.js
+++ advent_of_code/day_15.js
@@ -1,8 +1,19 @@
 var fs = require('fs');
 var rl = require('readline');
 
+var ingredientGenerator = function* () {
+  for (var i = 0; i < 101; i++) {
+    for (var j = 0; j < 101 - i; j++) {
+      for (var k = 0; k < 101 - i - j; k++) {
+        var l = 100 - i - j - k;
+        yield [i, j, k, l];
+      }
+    }
+  }
+}
+
 var partOne = function() {
-  var stream = fs.createReadStream('lines.txt');
+  var stream = fs.createReadStream('day_15.txt');
   var rl_stream = rl.createInterface({input: stream});
   var ingredients = {};
 
@@ -15,16 +26,51 @@ var partOne = function() {
     ingredients[ingredient]['flavor'] = parseInt(numbers[2]);
     ingredients[ingredient]['texture'] = parseInt(numbers[3]);
     ingredients[ingredient]['calories'] = parseInt(numbers[4]);
-    ingredients[ingredient]['ratio'] = 50;
   });
 
   rl_stream.on('close', function() {
-    console.log(ingredients);
-    for (var ingredient in ingredients) {
-      ingredient = ingredients[ingredient];
-      console.log(ingredient);
-    }
+    var categories = ['capacity', 'durability', 'flavor', 'texture'];
+    var total_points = 0;
+    var best_ratio;
+    var ratio;
+    var it = ingredientGenerator();
 
+    var points;
+    var a, b, c, d;
+    while (true) {
+      ratio = it.next().value;
+      if (ratio === undefined) {
+        break;
+      }
+      a = ratio[0];
+      b = ratio[1];
+      c = ratio[2];
+      d = ratio[3];
+      points = 1;
+      var calories = ingredients.Sugar.calories * a + 
+        ingredients.Sprinkles.calories * b +
+        ingredients.Candy.calories * c +
+        ingredients.Chocolate.calories * d;
+      if (calories !== 500) {
+        continue;
+      }
+      categories.forEach(function(category) {
+        var total = ingredients.Sugar[category] * a + 
+          ingredients.Sprinkles[category] * b +
+          ingredients.Candy[category] * c +
+          ingredients.Chocolate[category] * d;
+        if (total < 0) {
+          total = 0;
+        }
+        points *= total;
+      })
+      if (points > total_points) {
+        total_points = points;
+        best_ratio = ratio;
+      }
+    }
+    console.log(total_points);
+    console.log(best_ratio);
   });
 }
 partOne();
